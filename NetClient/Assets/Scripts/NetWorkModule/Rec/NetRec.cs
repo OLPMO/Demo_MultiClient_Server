10d149b042dfe3369e7cc83883d60fb96584c2ca
ZhangZhibin <1538933241@qq.com>
2017-07-28 17:22:13 +0800

    初版客户端代码
    需要永康完成NetSend.cs文件中的SendMessage函数，修改NetSend函数中的IP地址和端口号，
    需要永康完成NetRec.cs中的ReceiveMessage函数，修改NetRec函数中的IP以及端口号
    
    后面我会对代码结构以及各部分工作方式进行修改

---
+++ NetClient/Assets/Scripts/NetWorkModule/Rec/NetRec.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Assets.Scripts.Player;
+
+public class NetRec
+{
+    private static NetRec netRecInstance = null;
+    private Socket RecSocket;
+
+    public static NetRec GetSingleton()
+    {
+        if (netRecInstance == null)
+            netRecInstance = new NetRec();
+
+        return netRecInstance;
+    }
+
+    private NetRec()
+    {
+        RecSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //服务器信息
+        IPAddress ipServerAddress = IPAddress.Parse("10.20.73.61");
+        IPEndPoint ipServerEndPoint = new IPEndPoint(ipServerAddress, 10000);
+        IAsyncResult Result = RecSocket.BeginConnect(ipServerEndPoint, new AsyncCallback(connectCal), RecSocket);
+
+        bool success = Result.AsyncWaitHandle.WaitOne(5000, true);
+        if (!success)
+        {
+            Closed();
+            Debug.Log("超时");
+        }
+    }
+
+    public Vector3 ReceiveMessage()
+    {
+        //这里接受并解析出坐标信息
+        //RecSocket.Receive()
+        //返回坐标信息     
+
+        return new Vector3(0, 0, 0);                                                                                                                                                                                        
+    }
+
+
+
+    private void connectCal(IAsyncResult asyncConnect)
+    {
+        Debug.Log("connectSuccess");
+    }
+
+    //关闭Socket     
+    public void Closed()
+    {
+
+        if (RecSocket != null && RecSocket.Connected)
+        {
+            RecSocket.Shutdown(SocketShutdown.Both);
+            RecSocket.Close();
+        }
+        RecSocket = null;
+    }
+
+
+    public Vector3 RecTransform()
+    {
+        return new Vector3(0,0,0);
+    }
+
+
+}
