0a591b288a28cab486bae588f39112b2454e70c4
OLPMO <oolpmoo@163.com>
2017-07-29 23:22:12 +0800

    初步实现客户端玩家位置的打包解包

--- NetClient/Assets/Scripts/NetWorkModule/NetworkManager.cs
+++ NetClient/Assets/Scripts/NetWorkModule/NetworkManager.cs
@@ -11,7 +11,9 @@ namespace Assets.Scripts.NetWorkModule
     {
         private byte[] send = new byte[4096];
         private byte[] recv = new byte[4096];
-
+        private int nPackageType;
+        private long nPackageTime;
+        private int nUserId;
         private static readonly System.Object LockHelper = new System.Object();
         private  static NetworkManager networkManagerInstance = null;
 
@@ -47,10 +49,17 @@ namespace Assets.Scripts.NetWorkModule
         //这里向服务器发送Vector3变量，在这次Demo中这里是本地玩家的坐标信息
         public void SendMessageToServer(Vector3 obj)
         {
-            byte[] Temp = new byte[3];
-            Temp[0] = BitConverter.GetBytes(obj.x)[0];
-            Temp[1]= BitConverter.GetBytes(obj.y)[0];
-            Temp[2]= BitConverter.GetBytes(obj.z)[0];
+            byte[] Temp = new byte[100];
+            //包头：类型 | 时间戳 | 发送目标
+            //类型是与服务器约定好的
+            Temp[0] = BitConverter.GetBytes(1);
+            Temp[sizeof(int)] = BitConverter.GetBytes((long)13432);
+            Temp[sizeof(int)+sizeof(long)] = BitConverter.GetBytes(2);
+            //包的内容
+
+            Temp[sizeof(int) + sizeof(long)+sizeof(int)] = BitConverter.GetBytes(obj.x)[0];
+            Temp[sizeof(int) + sizeof(long) + 2*sizeof(int)] = BitConverter.GetBytes(obj.y)[0];
+            Temp[sizeof(int) + sizeof(long) + 3 * sizeof(int)] = BitConverter.GetBytes(obj.z)[0];
             int res=SocketManager.GetSingleton().GetSocket().Send(Temp);
         }
 
@@ -59,19 +68,32 @@ namespace Assets.Scripts.NetWorkModule
         {
             SocketManager.GetSingleton().GetSocket().Receive(recv);
         }
-
+     
+    
         //待完善---这里写接收信息的部分，注意线程的管理和内存的管理
+        
         public void  RecvMessageFromServer()
         {
             Thread thread = new Thread(recvFromServer);
+       
+            //解析包头
+            this.nPackageType = BitConverter.ToInt32(recv, 0);
+            this.nPackageTime = BitConverter.ToInt64(recv,sizeof(int));
+            this.nUserId = BitConverter.ToInt32(recv, sizeof(int)+sizeof(long));
         }
 
-
-        //待完善，这里负责解析信息中的坐标信息
+        //包头：类型 | 时间戳 | 发送目标
+        //待完善，这里负责解析信息中的坐标信息--志彬
+        //已实现解析包中坐标信息的功能 ---永康
         public Vector3 GetTransform()
         {
-            return new Vector3(0, 0, 0);
+            int nHeaderLen = sizeof(int)+sizeof(int)+sizeof(long);
+            float fX = (float)BitConverter.ToDouble(recv, nHeaderLen);
+            float fY = (float)BitConverter.ToDouble(recv, nHeaderLen+sizeof(double));
+            float fZ = (float)BitConverter.ToDouble(recv, nHeaderLen+2*sizeof(double));
+            return new Vector3(fX, fY, fZ);
         }
+
 
     }
 }
