cc63dbef2bb34c63f01a91c2d1edd2dfa23a875f
OLPMO <oolpmoo@163.com>
2017-07-29 10:46:40 +0800

    完成服务器数据库模块
    实现了用户校验api，接口如下：
    static bool UserValidate(const string name, const string password, int
    *id);

--- ServerLibs/ServDatabase.h
+++ ServerLibs/ServDatabase.h
@@ -2,5 +2,24 @@
 #define SERV_DATABASE_H
 
 
+#include <cstdio>
+#include <string>
+#include <mysql.h>
+#pragma comment(lib,"libmysql.lib")
+
+#define USERS_FIELD_NUM 3
+
+using namespace std;
+class ServDatabase{
+public:
+	static bool UserValidate(const string name, const string password, int *id);
+
+private:
+	static MYSQL mqServ2Db;
+	static MYSQL *pServ2DbSock;
+	static MYSQL *GetDbInstance();
+	static MYSQL *Connect2MySQL(char *pStrDbHost, char *pStrUserName, char *pStrUserPass, char *pStrDbName, int nPort);
+	static MYSQL *SetDbInstance(char *pStrDbHost, char *pStrUserName, char *pStrUserPass, char *pStrDbName, int nPort);
+};
 
 #endif
