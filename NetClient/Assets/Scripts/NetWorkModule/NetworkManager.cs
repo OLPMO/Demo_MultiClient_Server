3f9d289d0bb6433fe99cb43bb47d6674f4165c3e
ZhangZhibin <1538933241@qq.com>
2017-07-29 09:53:07 +0800

    添加注释
    本次Demo中需要的功能基本完成，需要永康进行数据接收部分的完善以及发送部分的完善

--- NetClient/Assets/Scripts/NetWorkModule/NetworkManager.cs
+++ NetClient/Assets/Scripts/NetWorkModule/NetworkManager.cs
@@ -33,19 +33,18 @@ namespace Assets.Scripts.NetWorkModule
             return networkManagerInstance;
         }
 
-
-
-
         public void SendMessageToServer(byte[] obj)
         {
             SocketManager.GetSingleton().GetSocket().Send(obj);
         }
 
+        //这里发送float变量
         public void SendMessageToServer(float obj)
         {
             SocketManager.GetSingleton().GetSocket().Send(BitConverter.GetBytes(obj));
         }
 
+        //这里向服务器发送Vector3变量，在这次Demo中这里是本地玩家的坐标信息
         public void SendMessageToServer(Vector3 obj)
         {
             byte[] Temp = new byte[3];
@@ -55,16 +54,20 @@ namespace Assets.Scripts.NetWorkModule
             int res=SocketManager.GetSingleton().GetSocket().Send(Temp);
         }
 
+
         private void recvFromServer()
         {
             SocketManager.GetSingleton().GetSocket().Receive(recv);
         }
 
+        //待完善---这里写接收信息的部分，注意线程的管理和内存的管理
         public void  RecvMessageFromServer()
         {
             Thread thread = new Thread(recvFromServer);
         }
 
+
+        //待完善，这里负责解析信息中的坐标信息
         public Vector3 GetTransform()
         {
             return new Vector3(0, 0, 0);
