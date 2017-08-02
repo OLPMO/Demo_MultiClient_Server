9c848dc492dfcd6bdab835947c5b81105831c7af
KantLeung <liangyongkang1@kingsoft.com>
2017-08-02 10:27:31 +0800

    时间头的时间改为全局时间控制器的接口

--- NetClient/Assets/Scripts/Network/DataPackage.cs
+++ NetClient/Assets/Scripts/Network/DataPackage.cs
@@ -11,7 +11,6 @@ namespace Assets.Scripts
     {
         public struct DataHead
         {
-            //这个由永康负责编写，这里保存了数据包的头的结构
             int nPakcageType;
             long ldTime;
             int nUid;
@@ -26,14 +25,11 @@ namespace Assets.Scripts
 
             this.nSizeOfDataHead = sizeof(DataHead);
             Data = new byte[nPackageSize];
-            //  PackageTool.AddReset();
-
+  
             this.dhPackageHead.nUid = nUid;
         }
 
-        public static int GetSizeOfDataHead()  
-        {
-            
+        public static int GetSizeOfDataHead()  {
             return sizeof(int)*2+sizeof(long);
         }
 
@@ -43,22 +39,22 @@ namespace Assets.Scripts
         }
 
         #region 这部分负责想发送的数据中添加用户数据，但是内部调用的是数据类型转换为byte型的工具函数
-        private DataPackage AddDataHead(DataHead dhPackageHead)
+        private DataPackage AddDataHead()
         {
-            TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
-            this.ldTime = Convert.ToInt64(ts.TotalMilliseconds);
-            //这部分永康完成
-            //永康需要
-            //  Array.Copy(BitConverter.GetBytes((double)62.5465), 0, recv, 2*sizeof(System.Int32) + 2*sizeof(double)+sizeof(System.Int64), sizeof(double));
-            Array.Copy(BitConverter.GetBytes(dhPackageHead.nPackageType),0,this.Data,0,sizeof(int));
-            Array.Copy(BitConverter.GetBytes(dhPackageHead.ldTime), 0, this.Data, sizeof(int), sizeof(long));
-            Array.Copy(BitConverter.GetBytes(dhPackageHead.nUid), 0, this.Data, sizeof(int)+sizeof(long), sizeof(int));
+            /*获取系统当前时间的毫秒时间戳，不精确
+             *  TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+             *  this.ldTime = Convert.ToInt64(ts.TotalMilliseconds);
+            */
+            this.dhPackageHead.ldTime = GlobalTimeController.sharedInstance().GetTimeSync();
+            Array.Copy(BitConverter.GetBytes(this.dhPackageHead.nPackageType),0,this.Data,0,sizeof(int));
+            Array.Copy(BitConverter.GetBytes(this.dhPackageHead.ldTime), 0, this.Data, sizeof(int), sizeof(long));
+            Array.Copy(BitConverter.GetBytes(this.dhPackageHead.nUid), 0, this.Data, sizeof(int)+sizeof(long), sizeof(int));
             return this;
         }
 
         public DataPackage AddHealth(int nHp)
         {
-            //  PackageTool.AddBaseType(ref Data,hud);
+           
             Array.Copy(BitConverter.GetBytes(nHp), 0, this.Data, this.nSizeOfDataHead, sizeof(int));
             return this;
         }
@@ -66,9 +62,8 @@ namespace Assets.Scripts
         public DataPackage AddPositon(Vector3 Pos)
         {
             this.dhPackageHead.nPakcageType = 0;
-            this.AddDataHead(this.dhPackageHead);
+            this.AddDataHead();
             
-           // PackageTool.AddVector(ref Data, Pos);
             float fX = (float)Pos.x;
             float fY = (float)Pos.y;
             float fZ = (float)Pos.z;
@@ -83,7 +78,7 @@ namespace Assets.Scripts
         }
         public DataPackage AddLoginInfo(string strName,string strPass) {
             this.dhPackageHead.nPakcageType = -1;
-            this.AddDataHead(this.dhPackageHead);
+            this.AddDataHead();
             strName += '\0';
             strPass += '\0';
             byte[] arrByteName = System.Text.Encoding.ASCII.GetBytes(strName);
@@ -92,15 +87,6 @@ namespace Assets.Scripts
             Array.Copy(arrBytePass, 0, this.Data, this.nSizeOfDataHead + sizeof(arrByteName), sizeof(arrBytePass));
             return this;
         }
-        public void PackPlayerUp()  //这些打包函数由永康完成
-        {
-            
-            //这里负责添加包的内容
-
-            //eg，先添加数据包头，然后在数据包中依次添加后面的数据
-            AddDataHead(1).AddHealth(2).AddPositon(new Vector3(0,0,0));
-
-        } 
         #endregion
 
 
